cc7b4d8d1c631b7506cfdeb425f5593a85ea0072
Dave Fernandes Dev <dave.fernandes.dev@gmail.com>
2022-03-27 21:46:09 -0300

    aula 3.7 done, retorna 404 quando não encontra o recurso

--- src/main/java/dev/fernandes/dave/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/dev/fernandes/dave/algamoney/api/resource/CategoriaResource.java
@@ -1,24 +1,44 @@
 package dev.fernandes.dave.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import dev.fernandes.dave.algamoney.api.model.Categoria;
 import dev.fernandes.dave.algamoney.api.repository.CategoriaRepository;
 
-@RestController @RequestMapping("/categories")
+@RestController
+@RequestMapping("/categories")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRespository;
 	
-	@GetMapping
-	public List<Categoria> findAll(){
-		return categoriaRespository.findAll();		
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Optional<Categoria>> findById(@PathVariable Integer id) {
+		Optional<Categoria> obj = categoriaRespository.findById(id);
+		return obj.isPresent() ? ResponseEntity.ok().body(obj) : ResponseEntity.notFound().build();
 	}
 
+	@GetMapping
+	public List<Categoria> findAll() {
+		return categoriaRespository.findAll();
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria objDTO) {
+		Categoria newObj = categoriaRespository.save(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).body(newObj);
+	}
 }
