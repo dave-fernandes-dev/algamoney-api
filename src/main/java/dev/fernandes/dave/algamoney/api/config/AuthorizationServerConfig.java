ee7752491cb57ead9f6de5139ee7cba1af281d25
Dave Fernandes Dev <dave.fernandes.dev@gmail.com>
2022-04-10 20:59:23 -0300

    concluída aula 6.5 oauth2 com jwt in memory

--- src/main/java/dev/fernandes/dave/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/dev/fernandes/dave/algamoney/api/config/AuthorizationServerConfig.java
@@ -10,8 +10,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -43,8 +46,16 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints
+			.accessTokenConverter(accessTokenConverter())
 			.authenticationManager(authenticationManager)
 			.tokenStore(tokenStore());
+	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter jatc = new JwtAccessTokenConverter();
+		jatc.setSigningKey("algaworks");
+		return jatc;
 	}
 
 	@Override
@@ -54,7 +65,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
 	@Bean
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		return new JwtTokenStore(accessTokenConverter());
 	}
 
 
