b01c58c2568f4c73cd4a746092eba8c1f499d92d
Dave Fernandes Dev <dave.fernandes.dev@gmail.com>
2022-04-21 17:51:13 -0300

    concluída aula 7.2. Profiles do Spring

--- src/main/java/dev/fernandes/dave/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/dev/fernandes/dave/algamoney/api/AlgamoneyApiApplication.java
@@ -2,8 +2,12 @@ package dev.fernandes.dave.algamoney.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import dev.fernandes.dave.algamoney.api.config.property.AlgamoneyApiProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AlgamoneyApiProperty.class)
 public class AlgamoneyApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/dev/fernandes/dave/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -0,0 +1,26 @@
+package dev.fernandes.dave.algamoney.api.config.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data 
+@ConfigurationProperties("algamoney")
+@Component
+public class AlgamoneyApiProperty {
+	
+	private String originPermitida = "http://localhost:8000"; 
+	
+	private final Seguranca seguranca = new Seguranca();
+	
+	@Data
+	public
+	static class Seguranca {
+		
+		private boolean enableHttps;
+		
+	}
+	
+
+}
