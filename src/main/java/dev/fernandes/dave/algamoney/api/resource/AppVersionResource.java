b901dc67770d9d5547da2768a9756f3b9021b140
Dave Fernandes Dev <dave.fernandes.dev@gmail.com>
2022-06-28 17:33:30 -0300

    concluída aula 22.18. Configurando o template para o envio do e-mail

--- src/main/java/dev/fernandes/dave/algamoney/api/resource/AppVersionResource.java
+++ src/main/java/dev/fernandes/dave/algamoney/api/resource/AppVersionResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/version")
 public class AppVersionResource {
+
 	
 	@GetMapping()
 	public String version() {
